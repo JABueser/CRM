b13773bc579e6f13a980de8325c7acafe1b3f82e
Yanling Zhang <lingzhang126@gmail.com>
2018-09-26 19:02:40 -0400

    created a temp view for the volunteer page, added a volunteer model

---
+++ CRM/CRM/Models/Volunteer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRM.Models
+{
+    public class Volunteer
+    {
+        public int volunteerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string interest { get; set; }
+    }
+}
