21fe70602b3afdcac0d5e48d6e429388a2867657
Yanling Zhang <lingzhang126@gmail.com>
2018-10-05 13:59:56 -0400

    edited statup

--- CRM/CRM/Startup.cs
+++ CRM/CRM/Startup.cs
@@ -16,6 +16,7 @@ namespace CRM
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
