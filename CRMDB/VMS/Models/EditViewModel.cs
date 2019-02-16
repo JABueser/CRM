0f88dfc5f58cf1806c8d90acd41b8b3d9fc77a9c
John Supina <jsupina32@aim.com>
2019-02-16 17:15:43 -0500

    refactor part 3 mostly models

---
+++ CRMDB/VMS/Models/EditViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VMS.Models
+{
+    public class EditViewModel
+    {
+        public List<int> SelectedCategories { get; set; }
+        public List<int> SelectedDays { get; set; }
+        public List<int> PreviousCategories { get; set; }
+        public List<int> PreviousDays { get; set; }
+        public List<Category> AllCategories { get; set; }
+        public List<Availability> AllAvailability { get; set; }
+        public Volunteer VolunteerPerson { get; set; }
+    }
+}
---
+++ CRMDB/VMS/Models/ViewHourViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VMS.Models
+{
+    public class ViewHourViewModel
+    {
+        public int Doctor { get; set; }
+        public int Dentist { get; set; }
+        public int Nurse { get; set; }
+        public int Ministry { get; set; }
+        public int Bus { get; set; }
+        public int Office { get; set; }
+        public int Maintenance { get; set; }
+        public int Auto { get; set; }
+        public int Food { get; set; }
+        public int Men { get; set; }
+        public int Thrift { get; set; }
+        public int Special { get; set; }
+        public int Women { get; set; }
+        public int Training { get; set; }
+        public int Total { get; set; }
+    }
+}
