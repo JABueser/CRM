d59304f07f52630382a17d53f76509c8d74f2eed
Kristen Wankowski <n01040816@ospreys.unf.edu>
2019-02-16 16:49:11 -0600

    Refactoring part 2 (VolunteersController) and (FilterViewModel)

---
+++ CRMDB/VMS/Models/FilterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VMS.Models
+{
+    public class FilterViewModel
+    {
+        public IEnumerable<Category> AllCategories { get; set; }
+
+        public string SelectedCategoryID { get; set; }
+
+        public HourViewModel HourViewModel { get; set; }
+    }
+}
