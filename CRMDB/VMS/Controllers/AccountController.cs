d12e88f053811d32b3b2b0074ea634259177da60
Yanling Zhang <lingzhang126@gmail.com>
2019-02-28 16:09:34 -0500

    formatting editing

--- CRMDB/VMS/Controllers/AccountController.cs
+++ CRMDB/VMS/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace VMS.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Home", "Index");
                 }
                 AddErrors(result);
             }
@@ -392,7 +392,7 @@ namespace VMS.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Account", "Login");
         }
 
         //
