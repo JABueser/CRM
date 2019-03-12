8c58ead64f8a0fb906a2c0e2d6dd4a9f4be71c94
Kristen Wankowski <n01040816@ospreys.unf.edu>
2019-03-12 12:44:41 -0500

    Changed somethings

--- CRMDB/VMS/Controllers/AccountController.cs
+++ CRMDB/VMS/Controllers/AccountController.cs
@@ -163,7 +163,7 @@ namespace VMS.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Home", "Index");
+                    return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
