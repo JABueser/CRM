dba1dc5ca73ab7e014964eb94de375277f05cd65
John Amiel Bueser <n00911402@ospreys.unf.edu>
2019-03-23 01:52:19 -0400

    Changed dropdown menu for the modal so that only categories related to volunteer is shown

--- CRMDB/VMS/Models/HourViewModel.cs
+++ CRMDB/VMS/Models/HourViewModel.cs
@@ -15,9 +15,9 @@ namespace VMS.Models
         public string Email { get; set; }
         public List<string> Categories { get; set; }
         public string Church { get; set; }
+        public string DateCreated { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
-        public string DateCreated { get; set; }
         public DateTime Date { get; set; }
         public int Hours { get; set; }
         public int CatName { get; set; }
