fafc511b5fe8e8da896d0b3ef6b5a64508d6bd3e
John Supina <jsupina32@aim.com>
2019-04-17 19:09:42 -0400

    css

--- CRMDB/VMS/Models/Volunteer.cs
+++ CRMDB/VMS/Models/Volunteer.cs
@@ -11,7 +11,8 @@ namespace VMS.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Volunteer
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -46,6 +47,8 @@ namespace VMS.Models
         public string Phone { get; set; }
         public int TotalHours { get; set; }
         public string Address2 { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MMMM dd, yyyy}")]
         public System.DateTime DateCreated { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
